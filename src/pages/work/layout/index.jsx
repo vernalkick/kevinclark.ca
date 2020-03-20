b6e021a71eb9837b6e967b4e6d0bba87ea608408
Kevin Clark <kevin@kevinclark.ca>
2020-03-20 17:00:33 -0400

    New portfolio piece

---
+++ src/pages/work/layout/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Section from '../../../components/Section'
+import {Grid, Column} from '../../../components/ContentGrid'
+import HeroImage from '../../../components/HeroImage'
+import Project from '../../../templates/project'
+import IpadPro from '../../../components/iPadPro'
+import styled from 'styled-components'
+import { device } from '../../../components/Media'
+
+import heroLight from './layout-light.png'
+import heroDark from './layout-dark.png'
+import pictures from './pictures.png'
+
+export const frontmatter = {
+  company: 'Personal Project',
+  title: "Layout",
+  image: './climate-hero.png',
+  date: '2015-10-27',
+  slug: 'layout',
+  websiteLink: "https://layout.fm"
+}
+
+const SecondColumn = styled(Column)`
+  @media ${device.desktopUp} {
+    margin-top: -57%;
+  }
+`
+
+const Layout = ({location}) => (
+  <Project frontmatter={frontmatter} location={location}>
+    <div>
+      <Section>
+        <HeroImage>
+          <IpadPro>
+            <picture>
+              <source srcset={heroDark} media="(prefers-color-scheme: dark)" />
+              <img src={heroLight} alt="Layout website screenshot" />
+            </picture>
+          </IpadPro>
+        </HeroImage>
+        </Section>
+        <Section>
+        <Grid>
+          <Column width={5}>
+            <h2>Layout is a podcast about design, technology and everything else. I’ve been lucky to be doing this podcast every week with my friend <a href="https://twitter.com/rafahari">Rafa</a>.</h2>
+          </Column>
+        </Grid>
+        <Grid>
+          <Column width={4}>
+            <p>For as long as I can remember I’ve been a fan of podcasts. I first started by listening to <a href="https://5by5.tv/talkshow">The Talk Show</a> on <a href="https://5by5.tv">5by5</a> and quickly discovered, <a href="https://5by5.tv/buildanalyze">Build & Analyse</a>, <a href="https://5by5.tv/hypercritical">Hypercritical</a>, <a href="https://podcasts.apple.com/us/podcast/irl-talk/id732909708">Geek Friday</a> and the many other shows on the network. To say podcasts were a revelation for me is a bit of an understatement.</p>
+            
+            <p>These people were smart, funny and nerdy in all of the right ways. And I felt like podcasts were able to get in depth in a way very few other mediums could. It would be cool to do that someday too, I thought.</p>
+          </Column>
+           <SecondColumn width={4} start={5}>
+            <img src={pictures} alt="Pictures of Layout stickers" />
+            <p>A few months later, Rafa reached out to me to know if I’d be interested in doing a podcast with him. We barely knew each other back then. <a href="https://twitter.com/rafahari">Rafa</a> was just someone I followed on Twitter. And he lived in Portugal. I couldn’t miss this opportunity to give this thing a shot.</p>
+            
+            <p>We decided we would record 3 episodes in secret and if either one of us didn’t like the result, we would stop there and never tell anyone that we’d even had the idea of doing a show together.</p>
+            
+            <p>5 years later, Layout is still going strong and I consider Rafa to be one of my best friends. It’s been a wild ride and allowed me to meet so many incredible people.</p>
+          </SecondColumn>
+        </Grid>
+
+      </Section>
+      
+    </div>
+  </Project>
+)
+
+export default Layout
--- src/templates/project.jsx
+++ src/templates/project.jsx
@@ -6,6 +6,7 @@ import MainPageHeader from '../components/MainPageHeader'
 import { device } from '../components/Media'
 import RelatedProjects from '../components/RelatedProjects'
 import AppStore from '../assets/appstore.svg'
+import Compass from '../assets/compass.svg'
 
 const PreTitle = styled.span`
   font-size: 20px;
@@ -17,18 +18,19 @@ const PreTitle = styled.span`
   }
 `
 
-const AppStoreLink = styled.a`
+const ProjectLink = styled.a`
   font-size: 18px;
   margin: 0;
   display: block;
   font-weight: 500;
   display: flex;
   align-items: center;
-  margin-top: 0.5rem;
+  margin-top: 1rem;
 `
 
-const AppStoreIcon = styled(AppStore)`
+const ProjectLinkIcon = styled.div`
   margin-right: 0.5rem;
+  display: flex;
 `
 
 export default ({ location, frontmatter, data, children }) => {
@@ -46,9 +48,20 @@ export default ({ location, frontmatter, data, children }) => {
       <MainPageHeader preTitle={frontmatter.company}>
         <PreTitle><strong>{frontmatter.company}</strong> — {frontmatter.date.substring(0,4)}</PreTitle>
         <h1>{frontmatter.title.prettify()}</h1>
-
       </MainPageHeader>
-      {frontmatter.appStoreLink && <AppStoreLink href={frontmatter.appStoreLink}><AppStoreIcon /> <span>Get it on the App Store →</span></AppStoreLink>}
+      
+      {frontmatter.appStoreLink && 
+        <ProjectLink href={frontmatter.appStoreLink}>
+          <ProjectLinkIcon><AppStore /></ProjectLinkIcon>
+          <span>Get it on the App Store →</span>
+        </ProjectLink>}
+        
+      {frontmatter.websiteLink &&
+        <ProjectLink href={frontmatter.websiteLink}>
+          <ProjectLinkIcon><Compass /></ProjectLinkIcon>
+          <span>View website →</span>
+        </ProjectLink>}
+
       {children}
 
       <RelatedProjects currentProjectSlug={frontmatter.slug} />
