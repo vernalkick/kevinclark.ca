61225abe30202210d64efc62d7700acb1c9dac56
Kevin Clark <kevin@kevinclark.ca>
2020-07-27 17:47:46 -0400

    Delete unnecesary text styles

--- src/components/TextStyles.jsx
+++ src/components/TextStyles.jsx
@@ -1,31 +1,4 @@
-import React from 'react'
 import styled from 'styled-components'
-
-export const CaptionText = styled.span`
-  color: var(--secondary-text-color);
-  font-size: 16px;
-  font-family: var(--secondary-font);
-  line-height: 1.5;
-  display: block;
-  position: relative;
-
-  &:before {
-    content: "";
-    display: ${props => props.decoration ? 'block' : 'none'};
-    position: absolute;
-    height: 140px;
-    width: 2px;
-    background: var(--subdued-elements-color);
-    transform: rotate(45deg) translateY(15%);
-    bottom: -2rem;
-    right: 1rem;
-    z-index: -1;
-  }
-
-  a {
-    border-bottom: 2px solid var(--subdued-elements-color);
-  }
-`
 
 export const Caption = styled.span`
   display: block;
